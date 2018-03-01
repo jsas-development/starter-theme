4341b87d1742d6d4b9cd00bb399b8c548d8340bc
leandro_mateus <leandro.mateus@engine.ag>
2018-03-01 10:29:33 -0300

    Adicionado Bootstrap 4 Grid

--- index.php
+++ index.php
@@ -31,10 +31,20 @@
 				    No arquivo styles.min.scss descomentar as linhas dos arquivos nos quais serão usados e/ou acrescentar novos que serão usados.
 				  </div>
 				</div>
-
-
 			</div>
 		</div>
+		<div class="row">
+			  <div class="col-md-4">.col-md-4</div>
+			  <div class="col-md-4 ml-auto">.col-md-4 .ml-auto</div>
+			</div>
+			<div class="row">
+			  <div class="col-md-3 ml-md-auto">.col-md-3 .ml-md-auto</div>
+			  <div class="col-md-3 ml-md-auto">.col-md-3 .ml-md-auto</div>
+			</div>
+			<div class="row">
+			  <div class="col-auto mr-auto">.col-auto .mr-auto</div>
+			  <div class="col-auto">.col-auto</div>
+			</div>
 	</div>
 </section>
 
