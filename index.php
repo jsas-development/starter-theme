f9e7cc9c8fead6eadde86a9f13318b567307ed40
Bruno Cruz <brunohenriquedacruz@hotmail.com>
2019-12-09 11:19:24 -0300

    ajustes

--- index.php
+++ index.php
@@ -8,6 +8,29 @@
 
 ?>
 
+<div class="webdoor">
+    <div class="container">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="logo-sbpsp col-md-2">
+                <picture>
+                    <source srcset="assets/images/logo.webp" type="image/webp">
+                    <source srcset="assets/images/logo.png" type="image/png"> 
+                    <img src="assets/images/logo.png" alt="Logo Sbpsp">
+                </picture>
+            </div>
+            <div class="nav-scroller">
+                <nav class="nav d-flex justify-content-between align-items-center col-md-6">
+                    <a class="p-2 text-light" href="#">World</a>
+                    <a class="p-2 text-light" href="#">U.S.</a>
+                    <a class="p-2 text-light" href="#">Technology</a>
+                    <a class="p-2 text-light" href="#">Design</a>
+                    <a class="p-2 text-light" href="#">Culture</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php 
 
