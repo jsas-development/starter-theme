93c1c10216f65bc13045db86329c5b203652254f
Bruno Cruz <brunohenriquedacruz@hotmail.com>
2020-01-14 15:57:17 -0200

    pagina artigo

---
+++ blog-artigo.php
@@ -0,0 +1,119 @@
+<?php
+
+    $title       = 'Starter Template!';
+    $description = 'v2.0.0';
+    $keywords    = 'Starter, Initial Template, Template de arranque';
+    
+    include('header.php');
+
+?>
+
+<section class="artigo">
+    <div class="container py-5">
+        <span class="artigo--subtitle">Artigo</span>
+        <h1 class="artigo--title">
+            Bacurau - O Brasil em Pontência
+        </h1>
+        <span class="artigo--subtitle">20 de Novembro de 2019 </span>
+        <div class="row">
+            <div class="col-12 py-5">
+                <picture>
+                    <source srcset="assets/images/bacurau-artigo.webp" type="image/webp">
+                    <source srcset="assets/images/bacurau-artigo.jpg" type="image/jpg"> 
+                    <img class="card-img-top" src="assets/images/bacurau-artigo.jpg" alt="Bacurau">
+                </picture>
+            </div>
+            <div class="col-12 p-5">
+                <div class="artigo--text">
+                    <p>Bacurau, longa metragem, dirigido pelos diretores pernambucanos Kleber Mendonça Filho e Juliano Dornelles, estreou no fim de agosto de 2019, nos cinemas do Brasil, depois de ter ganho o Prêmio do Júri no Festival de Cannes, e também eleito como o melhor filme do Festival de Cinema de Munique, na Alemanha, e do 23º Festival de Cinema de Lima, no Peru.
+
+                    <p>Aos que ainda não assistiram, não se preocupem com spoilers, Bacurau é um filme que mais interessa o “como” do que o “o quê”. Bacurau resistirá a qualquer spoiler.
+
+                    <p>A palavra que dá nome ao filme é a da cidade na qual a trama se desenrola, situada no interior de Pernambuco, em algum futuro incerto – o fato da história se passar em uma distopia futurista ela se apresenta em sinais delicados ao longo do filme – o nome da cidade é a de um pássaro do sertão, um pássaro forte, que só sai à noite.
+
+                    <p>Bacurau é pobre, sofre com falta de água, sofre com a ganância comum e conhecida de um político, que distribui comida vencida e livros como se fossem lixo, os moradores não têm a beleza convencional do cinema, peles marcadas pelo sol, chinelos nos pés, insetos voando dentro das casas, suor na testa do professor. Mas Bacurau não é apenas pobre, no começo do longa metragem, uma moça retorna à cidade de origem para o velório de sua avó. Avó negra, morta aos 94 anos, é contada como uma árvore anciã da qual descendem frutos, na voz de seu filho, que conduz o velório, Dona Carmelita teve filhos, netos, netas e bisnetos, médicos, professores, putas, arquitetos, pedreiros. Da avó idosa negra, uma linhagem de filhos negros, netos mestiços que se espalharam pelo Brasil e pelo mundo.
+
+                    <p>Em Bacurau, há um professor negro, há uma médica lésbica (interpretada por Sônia Braga), as crianças aprendem música, há biblioteca e há museu. A igreja serve de depósito – curiosa escolha a dos roteiristas (que também são os diretores), essa de deixar a igreja fechada.
+
+                    <p>Lunga, um anti-herói necessário, tido como criminoso e procurado pela justiça, é uma personagem curiosa, espécie de Lampião andrógino, vestido com acessórios dignos de um Mad Max do sertão.
+
+                    <p>O enredo da trama segue em ritmo diferente do conhecido, até certo ponto adiantado do filme, o telespectador se questiona do que se trata aquela história, porém, mesmo sem entregar às explicações sobre o enredo, o filme se desenrola com uma tensão constante e bem administrada.
+
+                    <p>Bacurau começa a sofrer ataques, pessoas são mortas. Um casal branco do Sudeste age juntamente a um grupo norte-americano. Em um dos diálogos, digno de clássico Tarantinesco, o casal brasileiro diz ser igual aos americanos: “Nós somos do Sudeste, de uma região rica de colônias alemãs e italianas, somos iguais a vocês”. Iguais? Ri um personagem norte-americano retratado como um red neck (expressão usada para designar caipiras norte-americanos, comumente violentos e pouco instruídos, típicos eleitores de Trump).
+
+                    <p>O casal de brasileiros brancos chega a Bacurau, os habitantes da cidade são gentis, mas desconfiados, o repentista da cidade nega o dinheiro oferecido pela mulher carioca revelando, por sua música, a arrogância e a prepotência dos brasileiros ricos. Bacurau é sertão pernambucano, é Brasil, mas também é “Game of Thrones”, Idade Média, o repentista – bobo da corte – denuncia, em piada, a suposta superioridade do Sul em relação ao Nordeste. Lunga, o fora da lei, vive em uma “torre”, armado e protegido, os norte-americanos, absolutamente convictos de estarem caçando mais animais do que gente, encarnam a superioridade colonialista opressora em relação às pessoas do terceiro mundo.
+
+                    <p>E, em meio a esse conjunto de referências, o sertanejo aparece como Euclides da Cunha os nomeou: antes de tudo, um forte.
+
+                    <p>Os personagens habitantes de Bacurau sorriem, simpáticos, em sua suposta ingenuidade infantil de cidadezinha de mentira. Um morador da cidade, homem negro, conhecedor de ervas e plantas, percebe o ataque eminente e se defende de seus agressores. Da parede do museu da cidade, as armas, da época de lampião, servem como proteção.
+
+                    <p>A violência, que tem ares de Tarantino, é avaliada pelos próprios personagens. Pacote – personagem central, um anti-herói mais para herói, menos fantasiado que Lunga – pergunta a uma das netas de Dona Carmelita:  Você não acha que Lunga exagerou? Não.
+
+                    <p>Tampouco sobra, para o espectador, violência sem sentido.
+
+                    <p>Bacurau é denúncia, é crítica, é suposição. Bacurau é um novo nordestino, potente. É um medo de para onde possamos caminhar. Uma lembrança do que pode voltar a ser.
+
+                    <p>Em um cortejo no qual o DJ narra nomes de vítimas mortas, os nomes, misturados a outros: Marielle, Marisa Letícia.
+
+                    <p>Filme oportuno ao momento político presente.
+                    
+                    <hr>
+
+                    <span>
+                        Por <u>Sociedade Brasileira de Psicanálise de São Paulo</u><br>
+                        Categoria: <u>Podcast</u>
+                    </span>
+
+                    <div class="d-flex">
+                        <a href="">ARTE</a>
+                        <a href="">CINEMA</a>
+                        <a href="">POLÍTICA</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="artigos-relacionados">
+    <div class="container">
+        <div class="col-12 text-center">
+            <h2 class="artigos-relacionados--title">Artigos relacionados</h2>
+        </div>
+        <div class="row py-5 mb-5">
+            <div class="col-4">
+                <a href="">
+                    <picture>
+                        <source srcset="assets/images/artigo-relacionado-1.webp" type="image/webp">
+                        <source srcset="assets/images/artigo-relacionado-1.jpg" type="image/jpg"> 
+                        <img class="card-img-top" src="assets/images/artigo-relacionado-1.jpg" alt="Bacurau">
+                    </picture>
+                </a>
+            </div>
+            <div class="col-4">
+                <a href="">
+                    <picture>
+                        <source srcset="assets/images/artigo-relacionado-2.webp" type="image/webp">
+                        <source srcset="assets/images/artigo-relacionado-2.jpg" type="image/jpg"> 
+                        <img class="card-img-top" src="assets/images/artigo-relacionado-2.jpg" alt="Bacurau">
+                    </picture>
+                </a>
+            </div>
+            <div class="col-4">
+                <a href="">
+                    <picture>
+                        <source srcset="assets/images/artigo-relacionado-3.webp" type="image/webp">
+                        <source srcset="assets/images/artigo-relacionado-3.jpg" type="image/jpg"> 
+                        <img class="card-img-top" src="assets/images/artigo-relacionado-3.jpg" alt="Bacurau">
+                    </picture>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php 
+
+   include('footer.php');
+    
+?>
