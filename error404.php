7a40ce9f0d4fa7ec86515d98f69766c3869e8cc4
Bruno Cruz <brunohenriquedacruz@hotmail.com>
2020-01-15 09:43:59 -0200

    pagina de erro

---
+++ error404.php
@@ -0,0 +1,21 @@
+<?php
+
+    $title       = 'Starter Template!';
+    $description = 'v2.0.0';
+    $keywords    = 'Starter, Initial Template, Template de arranque';
+    
+    include('header.php');
+
+?>
+
+<section class="error404 text-center py-5">
+    <span class="error404--number py-5">404</span>
+    <h1 class="error404--title py-5">A página que você esta procurando não existe.</h1>
+    <a class="error404--link btn-default my-5" href="/">VOLTAR PARA A PÁGINA INICIAL</a>
+</section>
+
+<?php 
+
+   include('footer.php');
+    
+?>
